3b7fcd371cd9642cb607c2a4ea560543f5669711
fernando.basello <>
2023-06-01 22:27:34 -0300

    Basic REPL

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
 extern crate monkey_lib;
 
+use std::io;
+
 use monkey_lib::lexer::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer = String::new();
+    let stdin = io::stdin(); // We get `Stdin` here.
+    println!("Input your code: ");
+    loop {
+        println!(">>>"); 
+        match stdin.read_line(&mut buffer) {
+            Ok(_) => {
+                let mut l = Lexer::new(buffer.trim());
+                loop {
+                    let token: token::Token = l.next_token();
+                    if token.tokentype == token::TokenTypes::EOF {
+                        break;
+                    }
+                    println!("{{{:?}, {}}}", token.tokentype, token.literal);
+                }
+            }
+
+            Err(error) => println!("error: {}", error),
+        }
+    }
 }
